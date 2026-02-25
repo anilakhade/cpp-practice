9ee7a618f08c641d9598a7c691de7f65821b964e
unknown <akhadeanil100@gmail.com>
2026-02-25 17:36:14 +0530

    solving...

---
+++ prob_021.cpp
@@ -0,0 +1,17 @@
+// Find second largest element
+
+#include <iostream>
+#include <vector>
+
+using ll = long long;
+
+ll FindSecondLargest(const std::vector<ll>& arr) {
+    
+}
+
+
+int main() {
+    std::vector<ll> v = {-9,0,5,3,-2,3,7};
+
+    return 0;
+}
