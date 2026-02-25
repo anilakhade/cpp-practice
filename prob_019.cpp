fd9363a0a47fd73ed215dc0c64122819a94f74d7
unknown <akhadeanil100@gmail.com>
2026-02-25 10:06:37 +0530

    prefix sum array solved

--- prob_019.cpp
+++ prob_019.cpp
@@ -3,9 +3,27 @@
 #include <iostream>
 #include <vector>
 
-void PrefixSumArray(const std::vector<int>& v)
+using ll = long long;
+std::vector<ll> PrefixSumArray(const std::vector<ll>& v) {
+    int n = v.size();
+    std::vector<ll> sum(n);
+    if (n == 0) {return sum;}
+    sum[0] = v[0];
+    
+    for (int i = 1; i < n; ++i) {
+        sum[i] = sum[i - 1] + v[i];
+    }
+    return sum;
+}
 
 int main() {
+    std::vector<ll> v = {10,20,30,40,50,60};
+    std::vector<ll> sum = PrefixSumArray(v);
+
+    for (int x : sum) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
