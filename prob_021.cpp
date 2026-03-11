6ef38186aec59c1e9018f840c62dd2856ed41c71
Anil Akhade <anilakhade@users.noreply.github.com>
2026-03-11 06:52:16 +0530

    find second largest element

--- prob_021.cpp
+++ prob_021.cpp
@@ -2,16 +2,33 @@
 
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using ll = long long;
 
 ll FindSecondLargest(const std::vector<ll>& arr) {
-    
+    if (arr.size() < 2) return -1;
+
+    ll first = LLONG_MIN;
+    ll second = LLONG_MIN;
+
+    for (ll num : arr) {
+        if (num > first) {
+            second = first;
+            first = num;
+        } else if (num > second && num != first) {
+            second = num;
+        }
+    }
+    return second;
 }
 
 
 int main() {
     std::vector<ll> v = {-9,0,5,3,-2,3,7};
+    ll a = FindSecondLargest(v);
+    std::cout << a << std::endl;
+
 
     return 0;
 }
