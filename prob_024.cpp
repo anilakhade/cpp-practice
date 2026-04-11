d30f3201682313236d0ba7122232483abe3fbb71
Anil Akhade <anilakhade@users.noreply.github.com>
2026-04-12 00:52:05 +0530

    Solved Count Frequency of All the Elements

---
+++ prob_024.cpp
@@ -0,0 +1,23 @@
+// Count the frequency of the all the elements
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+int main() {
+    std::vector<int> v = {1,1,2,2,3,3,3,4,5,5,5,6};
+    
+    //Declare map<KeyType, ValueType>
+    std::map<int, int> freq;
+    
+    //fill the map 
+    for (int x : v) {
+        freq[x]++;
+    }
+
+    for (auto const& [element, count] : freq) {
+        std::cout << element << " appears " << count << " times " << std::endl;
+    }
+
+    return 0;
+}
