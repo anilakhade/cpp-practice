1f1a8cfa2ce7906c8bcdcf4cd457ef26335d4051
Anil Akhade <anilakhade@users.noreply.github.com>
2026-05-19 21:26:50 +0530

    find duplicate number

--- prob_027.cpp
+++ prob_027.cpp
@@ -1,16 +1,26 @@
 // find duplicate numbers
 
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
-int DuplicateNumber(const std::vector<int>& v) {
-    int n = v.size();
+int FindDuplicate(const std::vector<int>& v) {
+    std::unordered_set<int> seen;
 
+    for (int x : v) {
+        if (seen.count(x)) return x;
+        seen.insert(x);
+    }
+    return -1;
 }
 
 int main() {
-    std::vector<int> v = {0,0,0,1,1,2,2,3,4,5};
+    std::vector<int> v = {0,1,2,0,4,5};
     
+    int duplicate = FindDuplicate(v);
+
+    std::cout << duplicate << std::endl;
+
 
     return 0;
 }
+    
+
